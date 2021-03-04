641e29223d2077c01ea3825e703d3988ade5d051
Carla Holcomb <carla.programmer@gmail.com>
2021-03-04 04:04:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,9 @@
 def my_each(collection) # put argument(s) here
   # code here
-  my_each(collection) do |i|
-  puts i
+  i = 0
+while i < collection.length
+  yield(collection[i])
+  i = i + 1
 end
+collection
 end
